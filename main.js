1ea22060a0d2006e75da46789eb38e6aea9c37ac
Viakure <nicolas.ipinzam@utem.cl>
2023-11-06 15:50:55 -0300

    Agregando lista productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
@@ -15,8 +16,8 @@ function toggleDesktopMenu() {
     if (!isAsideClose) {
         //Si asaid esta open , hay que cerrarlo
         aside.classList.add('inactive');
-    }
-    
+    };
+
     desktopMenu.classList.toggle('inactive');
 };
 function toggleMobileMenu() {
@@ -39,3 +40,61 @@ function toggleCarritoAside() {
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Screen',
+    price: 220,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Laptop',
+    price: 620,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+    for (produc of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', produc.img);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('div');
+        productPrice.innerText = '$' + produc.price;
+    
+        const productName = document.createElement('div');
+        productName.innerText = produc.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFig = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFig.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFig);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+};
+
+renderProducts(productList);
