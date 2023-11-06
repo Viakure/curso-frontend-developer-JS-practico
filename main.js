863b3012c13a7b27b580213ace6d42c84d4640e1
Viakure <nicolas.ipinzam@utem.cl>
2023-11-06 15:26:09 -0300

    Arreglando menus seguian abiertos

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
-const mobilepMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        //Si asaid esta open , hay que cerrarlo
+        aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 };
 function toggleMobileMenu() {
-    mobilepMenu.classList.toggle('inactive');
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        //Si asaid esta open , hay que cerrarlo
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
 };
+function toggleCarritoAside() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClose) {
+        //Si mobileMenu esta open , hay que cerrarlo
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
